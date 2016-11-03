e70d57334ab54972c5ffa3bdad4edfac837cebca
jmazrimas <joe.mazrimas@gmail.com>
2016-11-03 15:04:51 -0500

    basic testing working

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,14 +1,23 @@
 // Link.react-test.js
 import React from 'react';
 import TestComp from '../js/testcomponent';
-import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 
-test('Link changes the class when hovered', () => {
-  const component = renderer.create(
+// test('Link changes the class when hovered', () => {
+//   const component = renderer.create(
+//     <TestComp />
+//   );
+//   let tComp1 = component.toJSON();
+  
+//   console.log(tComp1)
+
+// });
+
+test('logic for testcomp', function() {
+  const wrapper = mount(
     <TestComp />
   );
-  let tComp1 = component.toJSON();
-  
-  console.log(tComp1)
 
-});
+  expect(wrapper.find('p').text()).toBe("Render this thing")
+
+})
